0ad4c2c58ab53e5fb013c9a4e07b6e13b533ec25
Jacquelinie <jacquelinechen555@gmail.com>
2024-09-19 03:17:47 +1000

    Completed

--- lab02/collections/src/main.rs
+++ lab02/collections/src/main.rs
@@ -9,11 +9,21 @@ fn main() {
     // VecDeque
     vec_deque_operations();
 
-    // TODO: your code here, for linked list insertions
+    // Linked List
+    linked_list_operations();
 
-    // TODO: your code here, for hashmap insertions
+    // Hashmap
+    hashmap_operations();
 
-    // TODO: your text explanation to the questions in the spec
+
+    // Questions
+    // Which collection type was the fastest for adding and removing elements?
+    // Why do you think this was the case?
+    // Is there any significant difference between Vec and VecDeque deletion?
+    // If so, why? If not, why not?
+    // When would you consider using VecDeque over Vec?
+    // When would you consider using LinkedList over Vec?
+    // Did the results suprise you? Why or why not?.
 }
 
 /// measure the insertion and removal
@@ -56,6 +66,51 @@ fn vec_deque_operations() {
     let time_start = std::time::Instant::now();
     for _ in 0..MAX_ITER {
         vec_deque.pop_front();
+    }
+    let time_end = std::time::Instant::now();
+
+    println!("remove: {:?}", time_end - time_start);
+}
+
+
+// Operations of Linked list
+fn linked_list_operations() {
+    let mut linked_list = LinkedList::new();
+
+    let time_start = std::time::Instant::now();
+    for i in 0..MAX_ITER {
+        linked_list.push_back(i);
+    }
+    let time_end = std::time::Instant::now();
+
+    println!("==== LinkedList ====");
+    println!("insert: {:?}", time_end - time_start);
+
+    let time_start = std::time::Instant::now();
+    for _ in 0..MAX_ITER {
+        linked_list.pop_front();
+    }
+    let time_end = std::time::Instant::now();
+
+    println!("remove: {:?}", time_end - time_start);
+}
+
+// Operations of Hash map
+fn hashmap_operations() {
+    let mut hashmap = HashMap::new();
+
+    let time_start = std::time::Instant::now();
+    for i in 0..MAX_ITER {
+        hashmap.insert(i, i);
+    }
+    let time_end = std::time::Instant::now();
+
+    println!("==== HashMap ====");
+    println!("insert: {:?}", time_end - time_start);
+
+    let time_start = std::time::Instant::now();
+    for i in 0..MAX_ITER {
+        hashmap.remove(&i);
     }
     let time_end = std::time::Instant::now();
 
