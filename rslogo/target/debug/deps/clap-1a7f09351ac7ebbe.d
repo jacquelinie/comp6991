e1eb188406ce8077437506b32474478005e47598
Jacqueline <126740004+jacquelinie@users.noreply.github.com>
2024-10-23 16:45:49 +1100

    Pushing

---
+++ rslogo/target/debug/deps/clap-1a7f09351ac7ebbe.d
@@ -0,0 +1,7 @@
+/Users/jacqueline/Desktop/comp6991/rslogo/target/debug/deps/libclap-1a7f09351ac7ebbe.rmeta: /Users/jacqueline/.cargo/registry/src/index.crates.io-6f17d22bba15001f/clap-4.5.20/src/lib.rs /Users/jacqueline/.cargo/registry/src/index.crates.io-6f17d22bba15001f/clap-4.5.20/src/../examples/demo.rs /Users/jacqueline/.cargo/registry/src/index.crates.io-6f17d22bba15001f/clap-4.5.20/src/../examples/demo.md
+
+/Users/jacqueline/Desktop/comp6991/rslogo/target/debug/deps/clap-1a7f09351ac7ebbe.d: /Users/jacqueline/.cargo/registry/src/index.crates.io-6f17d22bba15001f/clap-4.5.20/src/lib.rs /Users/jacqueline/.cargo/registry/src/index.crates.io-6f17d22bba15001f/clap-4.5.20/src/../examples/demo.rs /Users/jacqueline/.cargo/registry/src/index.crates.io-6f17d22bba15001f/clap-4.5.20/src/../examples/demo.md
+
+/Users/jacqueline/.cargo/registry/src/index.crates.io-6f17d22bba15001f/clap-4.5.20/src/lib.rs:
+/Users/jacqueline/.cargo/registry/src/index.crates.io-6f17d22bba15001f/clap-4.5.20/src/../examples/demo.rs:
+/Users/jacqueline/.cargo/registry/src/index.crates.io-6f17d22bba15001f/clap-4.5.20/src/../examples/demo.md:
