843080a82f97004b1550485b2ce845e2b29fa7a3
Jacqueline <126740004+jacquelinie@users.noreply.github.com>
2024-11-03 00:49:20 +1100

    Done

---
+++ lab08/webserver/target/debug/deps/webserver-d8b6dacf13ca462b.d
@@ -0,0 +1,7 @@
+/Users/jacqueline/Desktop/comp6991/lab08/webserver/target/debug/deps/webserver-d8b6dacf13ca462b: src/main.rs src/test.rs src/../index.html
+
+/Users/jacqueline/Desktop/comp6991/lab08/webserver/target/debug/deps/webserver-d8b6dacf13ca462b.d: src/main.rs src/test.rs src/../index.html
+
+src/main.rs:
+src/test.rs:
+src/../index.html:
---
+++ lab08/webserver/target/debug/webserver.d
@@ -0,0 +1,1 @@
+/Users/jacqueline/Desktop/comp6991/lab08/webserver/target/debug/webserver: /Users/jacqueline/Desktop/comp6991/lab08/webserver/index.html /Users/jacqueline/Desktop/comp6991/lab08/webserver/src/main.rs /Users/jacqueline/Desktop/comp6991/lab08/webserver/src/test.rs
