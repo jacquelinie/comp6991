3440bceece160aaea0406bf15740a10995e85e91
Jacqueline <126740004+jacquelinie@users.noreply.github.com>
2024-11-14 18:29:29 +1100

    Added vectors and matrix

--- rsheet/src/lib.rs
+++ rsheet/src/lib.rs
@@ -8,18 +8,19 @@ use rsheet_lib::cell_expr::{CellArgument, CellExpr, CellExprEvalError};
 use rsheet_lib::cells::{column_number_to_name, column_name_to_number};
 use std::error::Error;
 use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
+use std::sync::Mutex;
 use log::info;
 use std::process;
-use lazy_static::lazy_static;
+// use lazy_static::lazy_static;
 
 
 type CellMap = Mutex<HashMap<String, CellValue>>;
-type ExprMap = Mutex<HashMap<String, Arc<CellExpr>>>;
+type ExprMap = Mutex<HashMap<String, CellExpr>>;
 
-lazy_static! {
+// Initialize the cell map globally or within the server instance
+lazy_static::lazy_static! {
     static ref CELL_MAP: CellMap = Mutex::new(HashMap::new());
-    static ref EXPR_MAP: ExprMap = Mutex::new(HashMap::new());
+    static ref EXPR_MAP: CellMap = Mutex::new(HashMap::new());
 }
 
 pub fn start_server<M>(mut manager: M) -> Result<(), Box<dyn Error>>
@@ -104,7 +105,7 @@ fn new_cell_value(value: &str) -> CellValue {
 }
 
 // Function to parse the cell expression into the hashmap of cellvalues
-fn parse_expr_args(cell_expr: &CellExpr, cells: &HashMap<String, CellValue>) -> HashMap<String, CellArgument> {
+fn parse_expr_args(cell_expr: &CellExpr) -> HashMap<String, CellArgument> {
     // Check for args
     let vars = cell_expr.find_variable_names();
     if vars.is_empty() {
@@ -112,22 +113,99 @@ fn parse_expr_args(cell_expr: &CellExpr, cells: &HashMap<String, CellValue>) -> 
     }
 
     let mut results = HashMap::new();
+    let cells = CELL_MAP.lock().unwrap();
+
+    println!("VARS: {:?}", vars);
+
     for var in vars {
         // Value
-        let value = cells.get(&var).cloned().unwrap_or(CellValue::None);
+        let mut value = CellArgument::Value(CellValue::None);
 
+        // Matrix or Vector
+        if var.contains("_") {
+            let coords: Vec<&str> = var.split('_').collect();
+            if coords.len() == 2 {
+                // Extract rows and columns once
+                let row1 = coords[0].chars().next();
+                let row2 = coords[1].chars().next();
+                let col1 = &coords[0][1..];
+                let col2 = &coords[1][1..];
 
-        // Matrix
-        // if var.contains("_") {
-
-        // } else {
-
-        // }
-
-        // Vector
+                // Matrix: both row and column are different
+                if row1 != row2 && col1 != col2 {
+                    value = get_matrix(coords, &cells);
+                }
+                // Vector: either rows or columns are the same
+                else if row1 == row2 || col1 == col2 {
+                    value = get_vector(coords, &cells);
+                }
+            }
+        } else {
+            value = CellArgument::Value(get_value(&var, &cells));
+        }
+        // Insert
         results.insert(var, value);
     }
-    convert_to_arguments(&results)
+    results
+}
+
+// Get value from cell
+fn get_value(var: &str, cells: &HashMap<String, CellValue>) -> CellValue{
+    cells.get(var).cloned().unwrap_or(CellValue::None)
+}
+
+// Get vector of cells
+fn get_vector(coords: Vec<&str>, cells: &HashMap<String, CellValue>) -> CellArgument {
+    // Extract start and end coordinates
+    let start = coords[0];
+    let end = coords[1];
+
+    // Determine if we're dealing with a row or column vector
+    let start_row = start.chars().next().unwrap();
+    let end_row = end.chars().next().unwrap();
+    let start_col: i32 = start[1..].parse().unwrap();
+    let end_col: i32 = end[1..].parse().unwrap();
+
+    let mut vector_values = Vec::new();
+    if start_row == end_row {
+        // Row vector (iterate over columns in the same row)
+        for col in start_col..=end_col {
+            let coord = format!("{}{}", start_row, col);
+            vector_values.push(get_value(&coord, cells));
+        }
+    } else if start_col == end_col {
+        // Column vector (iterate over rows in the same column)
+        for row in start_row..=end_row {
+            let coord = format!("{}{}", row, start_col);
+            vector_values.push(get_value(&coord, cells));
+        }
+    }
+
+    CellArgument::Vector(vector_values)
+}
+
+// Get matrix of cells
+fn get_matrix(coords: Vec<&str>, cells: &HashMap<String, CellValue>) -> CellArgument {
+    // Extract start and end coordinates
+    let start = coords[0];
+    let end = coords[1];
+
+    let start_row = start.chars().next().unwrap();
+    let end_row = end.chars().next().unwrap();
+    let start_col: i32 = start[1..].parse().unwrap();
+    let end_col: i32 = end[1..].parse().unwrap();
+
+    let mut matrix_values = Vec::new();
+    for row in start_row..=end_row {
+        let mut row_values = Vec::new();
+        for col in start_col..=end_col {
+            let coord = format!("{}{}", row, col);
+            row_values.push(get_value(&coord, cells));
+        }
+        matrix_values.push(row_values);
+    }
+
+    CellArgument::Matrix(matrix_values)
 }
 
 // ===================== STAGE 1 ============================
@@ -154,8 +232,7 @@ fn handle_set(cell_identifier: &CellIdentifier, cell_expr: &str) ->  Option<Repl
 
     // Get cells
     let mut cells = CELL_MAP.lock().unwrap();
-    let mut exprs = EXPR_MAP.lock().unwrap();
-    let variables = parse_expr_args(&expr, &cells);
+    let variables = parse_expr_args(&expr);
 
     let result: Result<CellValue, CellExprEvalError> = expr.evaluate(&variables);
 
@@ -163,7 +240,6 @@ fn handle_set(cell_identifier: &CellIdentifier, cell_expr: &str) ->  Option<Repl
     match result {
         Ok(value) => {
             cells.insert(cell_address.clone(), value.clone());
-            exprs.insert(cell_address.clone(), expr);
             None
         }
         Err(e) => Some(Reply::Error(format!("Error with setting value: {:?}", e))),
